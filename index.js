fa85499960725b10bce71ba3201b1698f56a2a24
Gabriel Viox <gviox17@gmail.com>
2021-08-31 22:31:21 -0500

    commit tree 5eb7b678b27b74f92962eba741ba49a817a2e505

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const thankyouCards = [];
+function writeCards(names, surprise) {
+    for (let i = 0; i < names.length; i++){
+    thankyouCards.push (`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+    debugger;}
+    return thankyouCards;
+}
+function countDown(int) {
+    let i = 11;
+    while (i > 0){
+        i--
+        console.log(i);
+    }
+    return int;
+}
